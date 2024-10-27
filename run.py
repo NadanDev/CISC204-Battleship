563be07b39e9417b8920cc155e2ae502c633042d
Nathan Daneliak <NadanDevelop@gmail.com>
2024-10-27 17:39:40 -0400

    Simple case

--- run.py
+++ run.py
@@ -22,7 +22,6 @@ LOCATIONS2D = [
     ['31', '32', '33']
 ]
 
-# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
 @proposition(E)
 class Hit(object):
     def __init__(self, status, location) -> None:
@@ -32,41 +31,8 @@ class Hit(object):
         self.location = location
 
     def _prop_name(self):
-        return f"A.{self.data}"
+        return f"Hit({self.status}={self.location})"
 
-
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
-@proposition(E)
-class FancyPropositions:
-
-    def __init__(self, data):
-        self.data = data
-
-    def _prop_name(self):
-        return f"A.{self.data}"
-
-# Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")   
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
-# At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
-
-
-# Build an example full theory for your setting and return it.
-#
-#  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
-#  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
-#  what the expectations are.
 def example_theory():
 
     for i in range(len(boardSetup)):
